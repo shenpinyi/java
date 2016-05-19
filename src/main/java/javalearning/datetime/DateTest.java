c71d6f592a7f515f8b33b93bf3d9e16e5fe2f0f8
Jack Shen <jack.shen@tpgtelecom.com.au>
2016-05-19 15:59:39 +1000

    Some code to read and write file. Some code to operate Date.

--- src/main/java/javalearning/datetime/DateTest.java
+++ src/main/java/javalearning/datetime/DateTest.java
@@ -12,63 +12,64 @@ import java.util.TimeZone;
 public class DateTest {
 
 	public static void main(String[] args) {
-		
+
 		// Use System.currentTimeMillis to get millisecond of current
-//		test01();
-		
+		// test01();
+
 		// Test Date
-//		test02();
-		
+		// test02();
+
 		// Test Calendar
-//		test03();
-		
+		// test03();
+
 		// Test TimeZone
-//		test04();
-		
+		// test04();
+
 		// Test using TimeZone in Calendar
-//		test05();
-		
+		// test05();
+
 		// Format display
-//		test06();
-		
+		// test06();
+
 		// Test Locale
-//		test07();
+		// test07();
 
 		// Test utc time
-		test08();
+		// test08();
+
+		// Test 24 hour
+		test09();
 	}
-	
-	
+
 	public static void test01() {
-		System.out.println(System.currentTimeMillis()); //1463548793100
+		System.out.println(System.currentTimeMillis()); // 1463548793100
 	}
-	
-	
+
 	public static void test02() {
-		
+
 		// New a Date without parameter, we get current date
 		Date d1 = new Date();
 		System.out.println("New Date without parameter d1: " + d1);
-		
+
 		// New a Date using parameter, we get target date
 		Date d2 = new Date(System.currentTimeMillis() - 10000000);
 		System.out.println("New Date with parameter d2: " + d2);
-		
+
 		// Method after, before, compareTo
 		System.out.println("d1 after d2? " + d1.after(d2));
 		System.out.println("d1 before d2? " + d1.before(d2));
 		System.out.println("d1 comareTo d2? " + d1.compareTo(d2));
 		System.out.println("d1 eqals d2? " + d1.equals(d2));
-//		System.out.println("d1 - d2? " + (d1 - d2)); //not support
-		
+		// System.out.println("d1 - d2? " + (d1 - d2)); //not support
+
 		// Get millisecond from Date
 		System.out.println("Millisecond of d1: " + d1.getTime());
 		System.out.println("Millisecond of d2: " + d2.getTime());
-		
+
 	}
-	
+
 	public static void test03() {
-		
+
 		Calendar c = Calendar.getInstance();
 		System.out.println(c);
 		System.out.println(c.getTime());
@@ -82,64 +83,66 @@ public class DateTest {
 		System.out.println(c.get(Calendar.HOUR));
 		System.out.println(c.get(Calendar.SECOND));
 		System.out.println(c.get(Calendar.AM_PM));
-		
+
 		c.roll(Calendar.MINUTE, 20);
 		System.out.println(c.getTime());
-		
+
 		c.add(Calendar.MINUTE, -20);
 		System.out.println(c.getTime());
-		
+
 		c.set(Calendar.MONTH, Calendar.AUGUST);
 		System.out.println(c.getTime());
-		
+
 		c.set(Calendar.YEAR, 1300);
 		System.out.println(c.getTime());
 		System.out.println(c.getTimeInMillis());
-		
+
 		c.set(2017, 7, 13);
 		System.out.println(c.getTime());
-		
+
 		c.set(2050, 2, 19, 10, 23, 14);
 		System.out.println(c.getTime());
 	}
-	
+
 	public static void test04() {
 
 		// get all timezone ids
-//		for (String s : TimeZone.getAvailableIDs()) {
-//			System.out.println(s);
-//		}
-		
+		// for (String s : TimeZone.getAvailableIDs()) {
+		// System.out.println(s);
+		// }
+
 		// get timezone ids using millisecond offset
-//		for (String s : TimeZone.getAvailableIDs(10 * 1000 * 3600)) {
-//			System.out.println(s);
-//		}
-		
+		// for (String s : TimeZone.getAvailableIDs(10 * 1000 * 3600)) {
+		// System.out.println(s);
+		// }
+
 		// get timezone of local machine
 		System.out.println(TimeZone.getDefault().getID());
 		System.out.println(TimeZone.getDefault().getRawOffset());
 		System.out.println(TimeZone.getDefault().getDisplayName());
 		System.out.println(TimeZone.getTimeZone("Australia/Sydney"));
-		
+
 	}
-	
+
 	public static void test05() {
 		Calendar c = Calendar.getInstance(TimeZone.getTimeZone(TimeZone.getAvailableIDs(8 * 1000 * 3600)[0]));
 		c.set(Calendar.HOUR, 3);
 		c.set(Calendar.AM_PM, 1);
-		System.out.println(c.getTime()); // display the time in local machine, not in the timezone
+		System.out.println(c.getTime()); // display the time in local machine,
+											// not in the timezone
 		System.out.println(c.getTimeZone().getDisplayName());
-		System.out.println(c.get(Calendar.HOUR)); // get the time in the timezone
+		System.out.println(c.get(Calendar.HOUR)); // get the time in the
+													// timezone
 		System.out.println(c.get(Calendar.MINUTE));
 		System.out.println(c.get(Calendar.SECOND));
 		System.out.println(c.get(Calendar.AM_PM));
-		
+
 	}
-	
+
 	public static void test06() {
 		SimpleDateFormat sdf = new SimpleDateFormat("'Today is' dd-MM-yyyy, 'now is' HH:mm:ss, 'timezone is' Z");
 		System.out.println(sdf.format(new Date()));
-		
+
 		// convert string to date
 		try {
 			Date d = sdf.parse("Today is 18-05-2016, now is 16:48:12, timezone is +1000");
@@ -147,8 +150,7 @@ public class DateTest {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		// convert string to date using timezone
 		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMddHHmmss");
 		sdf2.setTimeZone(TimeZone.getTimeZone(TimeZone.getAvailableIDs(8 * 1000 * 3600)[0]));
@@ -159,35 +161,37 @@ public class DateTest {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void test07(){
-		
-//		for (Locale lcl : Locale.getAvailableLocales()) {
-//			System.out.println(lcl.getDisplayCountry() + " " + lcl.getDisplayLanguage()
-//			+ " "  + lcl.getCountry() + " " + lcl.getLanguage());
-//		}
-		
+
+	private static void test07() {
+
+		// for (Locale lcl : Locale.getAvailableLocales()) {
+		// System.out.println(lcl.getDisplayCountry() + " " +
+		// lcl.getDisplayLanguage()
+		// + " " + lcl.getCountry() + " " + lcl.getLanguage());
+		// }
+
 		Locale lcl = Locale.getDefault();
 		ResourceBundle bundle = ResourceBundle.getBundle("datetime", lcl);
 		ResourceBundle bundle2 = ResourceBundle.getBundle("datetime", Locale.getAvailableLocales()[0]);
 		System.out.println(bundle.getString("hello"));
 		System.out.println(bundle2.getString("hello"));
-		
+
 		System.out.println(bundle.getString("msg"));
 		System.out.println(bundle2.getString("msg"));
-		
-		System.out.println(MessageFormat.format(bundle.getString("msg"), "Jack", new SimpleDateFormat("YYYY-MM-dd").format(new Date())));
-		System.out.println(MessageFormat.format(bundle2.getString("msg"), "Jack", new SimpleDateFormat("YYYY-MM-dd").format(new Date())));
-		
+
+		System.out.println(MessageFormat.format(bundle.getString("msg"), "Jack",
+				new SimpleDateFormat("YYYY-MM-dd").format(new Date())));
+		System.out.println(MessageFormat.format(bundle2.getString("msg"), "Jack",
+				new SimpleDateFormat("YYYY-MM-dd").format(new Date())));
+
 	}
-	
-	
+
 	public static void test08() {
 
 		// UTC
 		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
 		sdf1.setTimeZone(TimeZone.getTimeZone("UTC"));
-		
+
 		// Local
 		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
 
@@ -205,7 +209,7 @@ public class DateTest {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			Date d1 = sdf1.parse("2015-06-04T03:55:26");
 			Date d2 = sdf2.parse("2015-06-04T03:55:26");
@@ -216,10 +220,38 @@ public class DateTest {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
+
+	private static void test09() {
+		String input = "20160510120036:453564";
+		String str = "";
+		String Year = input.substring(0, 4);
+		String Month = input.substring(4, 6);
+		String Day = input.substring(6, 8);
+
+		String Hour = input.substring(8, 10);
+		String Mins = input.substring(10, 12);
+		String Sec = input.substring(12, 14);
+		String MilliSec = input.substring(15, 18); // "000";
+
+		String DateTime = Year + "-" + Month + "-" + Day + " " + Hour + ":" + Mins + ":" + Sec + ":" + MilliSec;
+
+		try {
+
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+			// sdf.setTimeZone(TimeZone.getTimeZone("GMT+11")); // modified by
+			// jack.shen to align with DW, keep the timestamp as it be
+			Date date = sdf.parse(DateTime);
+			long timeInMillisSinceEpoch = date.getTime();
+			System.out.println(date);
+
+			str = String.valueOf(timeInMillisSinceEpoch);
+
+		} catch (Exception e) {
+
+		}
+		System.out.println(str);
+	}
 
 }
---
+++ src/main/java/javalearning/io/IoTest.java
@@ -0,0 +1,177 @@
+package javalearning.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IoTest {
+
+	public static void main(String[] args) {
+//		sortDw();
+		sortOutput();
+	}
+	
+	private static void sortDw() {
+		String path = "D:\\99 Projects\\04 Amdocs\\BMS\\task001\\UnitTesting\\DW_Files";
+		int fisrtLineIgnore = 1; // lines should be ignored at beginning
+		int lastLineIgnore = 0;  // lines should be ignored at last
+		int[] columns = {4, 1};
+		int[] sorts = {0, 1}; // 0, asc, 1, desc
+		
+		// list files with filter
+		File[] files1 = listFiles(path, "^((?!lcf).)*\\.csv$");
+		for (int i = 0; i < files1.length; i ++) {
+			System.out.println(files1[i]);
+			sortCsv(files1[i].getAbsolutePath(), fisrtLineIgnore, lastLineIgnore, columns, sorts);
+		}
+	}
+	
+	private static void sortOutput() {
+		String path = "D:\\99 Projects\\04 Amdocs\\BMS\\task001\\UnitTesting\\Output_Files";
+		int fisrtLineIgnore = 1; // lines should be ignored at beginning
+		int lastLineIgnore = 0;  // lines should be ignored at last
+		int[] columns = {5, 2};
+		int[] sorts = {0, 1}; // 0, asc, 1, desc
+		
+		// list files with filter
+		File[] files1 = listFiles(path, "^((?!lcf).)*\\.csv$");
+		for (int i = 0; i < files1.length; i ++) {
+			System.out.println(files1[i]);
+			sortCsv(files1[i].getAbsolutePath(), fisrtLineIgnore, lastLineIgnore, columns, sorts);
+		}
+	}
+	
+	
+	
+	private static File[] listFiles(String path, String regex) {
+		File folder = new File(path);
+		FilenameFilter filter = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				if (name.matches(regex)) {
+					return true;
+				}
+				return false;
+			}
+		};
+		return folder.listFiles(filter);
+	}
+	
+	private static void sortCsv(String fileName, int fisrtLineIgnore,
+			int lastLineIgnore, int[] columns, int[] sorts) {
+		
+		List <String> lines = null;
+		
+		List <String> head = new ArrayList<>();
+		List <String> tail = new ArrayList<>();
+		
+		try {
+			lines = Files.readAllLines(Paths.get(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < lines.size() && i < fisrtLineIgnore; i++) {
+			head.add(lines.remove(0));
+		}
+		
+		for (int i = 0; i < lines.size() && i < lastLineIgnore; i++) {
+			tail.add(lines.remove(lines.size() - 1));
+		}
+		
+		List<String[]> list = new ArrayList<>();
+		for (int i = 0; i < lines.size(); i ++) {
+			list.add(lines.get(i).split(","));
+		}
+		
+		list = sortCsv(columns, sorts, 0, list);
+		StringBuffer buff = new StringBuffer();
+		
+		for (String[] fields : list) {
+			for (int i = 0; i < fields.length - 1; i ++) {
+				buff.append(fields[i] + ",");
+			}
+			buff.append(fields[fields.length - 1] + "\r\n");
+		}
+		
+		try {
+			File csv = new File(fileName+".parse");
+
+			BufferedWriter bw = new BufferedWriter(new FileWriter(csv), 500 * 1024);
+			bw.write(buff.toString());
+			bw.close();
+			System.out.println(csv.getAbsolutePath());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static List<String[]> sortCsv(int[] columns, int[] sorts, int idx, List<String[]> lines) {
+		
+		if (idx >= columns.length) {
+			return lines;
+		}
+		
+		int column = columns[idx];
+		int sort = sorts[idx];
+		
+		if (lines.size() <= 1) {
+			return lines;
+		}
+		
+		Comparator <String> comparator;
+		
+		if (sort == 0) {
+			comparator = new Comparator<String> () {
+				@Override
+				public int compare(String o1, String o2) {
+					return o1.compareTo(o2);
+				}
+			};
+		} else {
+			comparator = new Comparator<String> () {
+				@Override
+				public int compare(String o1, String o2) {
+					return o2.compareTo(o1);
+				}
+			};
+		}
+		
+		Map<String, List<String[]>> m = new TreeMap<>(comparator);
+		
+		for (String[] fields : lines) {
+			if (m.containsKey(fields[column])) {
+				m.get(fields[column]).add(fields);
+			} else {
+				
+				List<String[]> subs = new ArrayList<>();
+				subs.add(fields);
+				m.put(fields[column], subs);
+			}
+		}
+		
+		List<String[]> sortedList = new ArrayList<>();
+		
+		for (String key : m.keySet()) {
+			List<String[]> sub = m.get(key);
+			List<String[]> sortedSub = sortCsv(columns, sorts, idx + 1, sub);
+			for (String[] fields : sortedSub) {
+				sortedList.add(fields);
+			}
+		}
+		return sortedList;
+	}
+}
